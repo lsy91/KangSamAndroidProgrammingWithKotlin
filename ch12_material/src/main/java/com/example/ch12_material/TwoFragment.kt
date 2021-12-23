627c9196daf42677953b9b241bf12b4b1af67d2c
lsy91 <lsysysy91@gmail.com>
2021-12-23 22:58:27 +0900

    Ch12_Meterial

--- ch12_material/src/main/java/com/example/ch12_material/TwoFragment.kt
+++ ch12_material/src/main/java/com/example/ch12_material/TwoFragment.kt
@@ -1,20 +1,22 @@
-package com.example.ch11_jetpack
-
+package com.example.ch12_material
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ch11_jetpack.databinding.FragmentThreeBinding
 
-class ThreeFragment : Fragment() {
+
+class TwoFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return FragmentThreeBinding.inflate(inflater, container, false).root
+        return inflater.inflate(R.layout.fragment_two, container, false)
     }
+
+
 }
